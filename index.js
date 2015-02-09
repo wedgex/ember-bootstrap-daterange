b67116278de0771a282d2055d656132fc7e38664
Hunter Haydel <haydh530@gmail.com>
2015-02-09 15:04:32 -0600

    Add dependencies.

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-bootstrap-daterange'
+  name: 'ember-bootstrap-daterange',
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/moment/moment.js');
+    app.import(app.bowerDirectory + '/bootstrap-daterangepicker/daterangepicker.js');
+    app.import(app.bowerDirectory + '/bootstrap-daterangepicker/daterangepicker-bs3.css');
+  }
 };
